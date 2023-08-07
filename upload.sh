f0cdd5c79bfa254a4525e3eb2a1924a7fc9a0130
snakepat <56012044+snakepat@users.noreply.github.com>
2023-08-07 16:28:41 +0800

    Add files via upload
    配合aria2

---
+++ upload.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+LOG_FILE="upload.log"
+
+CHECK_CORE_FILE() {
+    CORE_FILE="$(dirname $0)/core"
+    if [[ -f "${CORE_FILE}" ]]; then
+        . "${CORE_FILE}"
+    else
+        echo "!!! core file does not exist !!!"
+        exit 1
+    fi
+}
+
+DEFINITION_PATH() {
+    LOCAL_PATH="${TASK_PATH}"
+    if [[ -f "${TASK_PATH}" ]]; then
+        REMOTE_PATH="${DRIVE_NAME}:${DRIVE_DIR}${DEST_PATH_SUFFIX%/*}"
+    else
+        REMOTE_PATH="${DRIVE_NAME}:${DRIVE_DIR}${DEST_PATH_SUFFIX}"
+    fi
+}
+
+UPLOAD_TEST(){
+    # echo "hello,world" >> "$LOG_FILE"
+    # echo "${TASK_PATH}" >> "$LOG_FILE"
+    python3 upload.py "${TASK_PATH}"
+}
+
+# echo "try again" >> "$LOG_FILE" 
+
+CHECK_CORE_FILE "$@"
+CHECK_PARAMETER "$@"
+CHECK_FILE_NUM
+CHECK_SCRIPT_CONF
+GET_TASK_INFO
+GET_DOWNLOAD_DIR
+CONVERSION_PATH
+UPLOAD_TEST
+CLEAN_UP
+exit 0
